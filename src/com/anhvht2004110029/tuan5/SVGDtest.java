1958ac95eaf5f4d7e5002ba5c138148b3d5a0dba
tuananh27â <vuhoangtuananh2710@gmail.com>
2021-10-10 19:47:58 +0700

    nộp bài tuần 05

---
+++ src/com/anhvht2004110029/tuan5/SVGDtest.java
@@ -0,0 +1,111 @@
+package com.anhvht2004110029.tuan5;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SVGDtest {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<SVGD> svgds = new ArrayList<>();
+   
+    public static void main(String[] args) {
+        
+        do {
+            menu();
+            System.out.println("Nhập Lựa Chọn:");
+            int n = sc.nextInt();
+           switch (n) {
+                case 0:
+                break;
+                case 1:
+                nhapDanhSach();
+                break;
+                case 2:
+                xuatDanhSachSinhVien();
+                break;
+                case 3:
+                timSinhVienTheoHoTen();
+                break;
+                case 4:
+                timVaXoaSinhVienTheoHoTen();
+                break;
+                case 5:
+                xoaTheoDiem();
+                break;
+               default: System.out.println("Lựa Chọn Không Hợp Lệ!");
+                   break;
+           }
+        } while (true);
+    }
+    private static void menu()
+    {
+        System.out.println("|-----------------------MENU-----------------------|");
+        System.out.println("|1Nhập Sinh Viên                                   |");
+        System.out.println("|2Xuất Thông Tin                                   |");
+        System.out.println("|3Xuất Danh Sách Sinh Viên Theo Điểm               |");
+        System.out.println("|4Tìm Sinh Viên Theo Tên                           |");
+        System.out.println("|5Tìm Và Xóa Sinh Viên Theo Tên                    |");
+        System.out.println("|6Tìm Và Xóa Sinh Viên Theo Điểm                   |");
+        System.out.println("|7Thoát                                            |");
+        System.out.println("|--------------------------------------------------|");
+    }
+    private static void nhapDanhSach(){
+        System.out.println("Nhập số lượng sinh viên muốn thêm: ");
+        int n = sc.nextInt();
+        System.out.println("Nhập danh sách sinh viên: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Sinh viên thứ "+ (i+1));
+            SVGD svgd = new SVGD();
+            svgd.nhapThongTin();
+            svgds.add(svgd);
+        }
+    }
+    public static void xuatDanhSachSinhVien() {
+        int i = 1;
+
+        System.out.println("==========Thông Tin Sinh Viên=========");
+        for (SVGD svgd : svgds) {
+            System.out.println("Sinh Viên Thứ " + i);
+            svgd.inThongTin();
+            i++;
+        }
+    }
+    
+    public static void timSinhVienTheoHoTen() {
+        sc.nextLine();
+        System.out.println("Nhập họ tên: ");
+        String name = sc.nextLine();
+        for (SVGD svgd : svgds) {
+            if (name.equals(name)){
+                svgd.inThongTin();
+                break;
+            }
+        }   
+            
+    }
+    public static void timVaXoaSinhVienTheoHoTen() {
+        sc.nextLine();
+        System.out.println("Nhập họ tên cần tìm và xóa: ");
+        String name = sc.nextLine();
+        for (SVGD svgd : svgds) {
+            if (name.equals(name)){
+                svgds.remove(name); 
+                svgd.inThongTin();
+                break;
+            }
+    }     
+    }
+    public static void xoaTheoDiem()
+    {
+        sc.nextLine();
+        System.out.println("Nhập Điểm Muốn Xoá:");
+        double d = sc.nextDouble();
+        for (SVGD svgd : svgds) {
+            if(svgd.diemTB == d)
+            {
+                svgds.remove(svgd);
+            }
+            break;
+        }
+    }     
+    
+}
