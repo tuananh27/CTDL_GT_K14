90e5fafde4d118cb3ef34b1d09bf34bd901552e5
tuananh27 <vuhoangtuananhgmail.com>
2021-12-07 20:51:39 +0700

    test

--- src/com/anhvht2004110029/tuan01/hellome.java
+++ src/com/anhvht2004110029/tuan01/hellome.java
@@ -3,6 +3,7 @@ package com.anhvht2004110029.tuan01;
 public class hellome {
     public static void main(String[] args) {
         System.out.println("hello master");
+        System.out.println("hello");
     }
     
 }
